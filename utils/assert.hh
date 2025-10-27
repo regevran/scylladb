b970bb3fba471bc97b60609c97b0e7272d720006
Ran Regev <ran.regev@scylladb.com>
2025-10-27 06:22:18 +0200

    assert: SCYLLA_CONTRACT_ASSERT

--- utils/assert.hh
+++ utils/assert.hh
@@ -6,4 +6,17 @@
 #include <cassert>
 
 /// Like assert(), but independent of NDEBUG. Active in all build modes.
-#define SCYLLA_ASSERT(x) do { if (!(x)) { __assert_fail(#x, __FILE__, __LINE__, __PRETTY_FUNCTION__); } } while (0)
+#define SCYLLA_ASSERT(x) \
+    do { \
+        if (!(x)) { \
+            __assert_fail(#x, __FILE__, __LINE__, __PRETTY_FUNCTION__); \
+        } \
+    } while (0)
+
+//// Mimick SCYLLA_ASSERT but for contract assertions
+#define SCYLLA_CONTRACT_ASSERT(x) \
+    do { \
+        contract_assert [[clang::contract_semantic("enforce")]] (x); \
+    } while(0)
+
+
